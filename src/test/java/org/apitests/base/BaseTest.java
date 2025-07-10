83a117869469068e81eb0d14eb375e66c46a903c
jaabless <jefferyasamoahbless@gmail.com>
2025-07-10 23:29:30 +0000

    feat: Add Request and Respnse Specification and AllureRestAssured to BaseTest

--- src/test/java/org/apitests/base/BaseTest.java
+++ src/test/java/org/apitests/base/BaseTest.java
@@ -1,17 +1,39 @@
 package org.apitests.base;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.lessThan;
+
 public abstract class BaseTest {
+    protected static RequestSpecification requestSpec;
+    protected static ResponseSpecification responseSpec;
+
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-        RestAssured.filters(
-                new RequestLoggingFilter(LogDetail.ALL),
-                new ResponseLoggingFilter(LogDetail.ALL));
+        // Common request specification
+        requestSpec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .addFilter(new AllureRestAssured())
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+
+        // Common response specification
+        responseSpec = new ResponseSpecBuilder()
+                .expectHeader("Content-Type", containsString("application/json"))
+                .expectResponseTime(lessThan(2000L))
+                .build();
     }
 }
