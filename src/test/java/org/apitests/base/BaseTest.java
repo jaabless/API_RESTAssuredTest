cfbe95bcf124af7fd330f5fb59452b2cd308f3b2
jaabless <jefferyasamoahbless@gmail.com>
2025-07-13 22:37:00 +0000

    feat: Add BaseTest class with common REST Assured setup and specifications

--- src/test/java/org/apitests/base/BaseTest.java
+++ src/test/java/org/apitests/base/BaseTest.java
@@ -4,7 +4,6 @@ import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
