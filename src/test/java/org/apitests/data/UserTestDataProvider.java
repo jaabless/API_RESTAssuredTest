ed683823f584c450551ba15b1c4dc7bf8f78adcf
jaabless <jefferyasamoahbless@gmail.com>
2025-07-14 08:18:44 +0000

    feat: Add UserTestDataProvider for /users endpoint test data

--- src/test/java/org/apitests/data/UserTestDataProvider.java
+++ src/test/java/org/apitests/data/UserTestDataProvider.java
@@ -4,7 +4,6 @@ package org.apitests.data;
 import org.apitest.models.User;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
-
 import java.util.stream.Stream;
 
 public class UserTestDataProvider implements ArgumentsProvider {
